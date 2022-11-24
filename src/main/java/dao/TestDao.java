810e15731fc05523c61386e9fdd98d673467a6c0
isshosng <purehug@hanmail.net>
2022-11-24 11:44:49 +0900

    OracleOCI 설정

--- src/main/java/dao/TestDao.java
+++ src/main/java/dao/TestDao.java
@@ -20,7 +20,7 @@ public class TestDao {
 		try {
 			Context ctx = new InitialContext();
 			Context envContext = (Context) ctx.lookup("java:/comp/env");
-			dataFactory = (DataSource) envContext.lookup("jdbc/oracleTHIN");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracleOCI");
 
 		} catch (Exception e) {
 			e.printStackTrace();
