e091fe0eb7e9a794e3375be0023c74ee70681181
isshosng <purehug@hanmail.net>
2022-11-24 11:55:55 +0900

    Oracle OCI set

--- src/main/java/dao/TestDAO.java
+++ src/main/java/dao/TestDAO.java
@@ -20,7 +20,7 @@ public class TestDAO {
 		try {
 			Context ctx = new InitialContext();
 			Context envContext = (Context) ctx.lookup("java:/comp/env");
-			dataFactory = (DataSource) envContext.lookup("jdbc/oracle07");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracleOCI");
 
 		} catch (Exception e) {
 			e.printStackTrace();
