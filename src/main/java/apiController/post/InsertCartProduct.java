7a154e2baf5083eab6713cc82ac6bf76d3749cc2
woowang789 <wangjh789@gmail.com>
2022-11-27 17:13:59 +0900

    add cart event

--- src/main/java/apiController/post/InsertCartProduct.java
+++ src/main/java/apiController/post/InsertCartProduct.java
@@ -16,6 +16,12 @@ public class InsertCartProduct implements ApiControllerInter{
 	@Override
 	public void process(String body, Map<String, String> paramMap, Map<String, Object> model) throws ParseException {
 		JSONObject json = (JSONObject) parser.parse(body);
+		int optnId = 
+				Integer.parseInt(json.get("optnId").toString());
+		int stock = Integer.parseInt(json.get("stock").toString());
+		String userId = json.get("userId").toString();
+		// todo : 장바구니 insert update 하나의 프로시저로 묶기
+		// & 관심 상품 추가/ 제거 마찬가지로
 		
 		
 	}
